e22f09f34d898af4c1e6913ac16d0b3eb5c2eaae
Burak Uğraş <99265170+burakugras@users.noreply.github.com>
2024-01-26 19:53:55 +0300

    Add files via upload
    reupload

---
+++ DesignPatternOdev1/Program.cs
@@ -0,0 +1,59 @@
+﻿using Odev_Lib;
+using System;
+
+namespace DesignPatternNOTEPAD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Note textNote=NoteFactory.GetTextNote("Başlık 1")
+            //                         .AddParagraph("Paragraf 1")
+            //                         .AddParagraph("Paragraf 2") 
+            //                         .Create();
+
+            //Console.WriteLine(textNote.View());
+
+            //------------------------------------------------------------------------------------------
+
+            //Note tableNote = NoteFactory.GetTableNote()
+            //                          .AddColumn("Ad")
+            //                          .AddColumn("Soyad")
+            //                          .AddRow("Burak", "Uğraş")
+            //                          .AddRow("Cengiz", "Öder").Create();
+
+            //Console.WriteLine(tableNote.View());
+
+            //------------------------------------------------------------------------------------------
+
+            Note listNote = NoteFactory.GetListNote()
+                                     .IsOrdered(false)
+                                     //.SetFormat(OrderedListTypes.Letter)
+                                     .SetCharacter(UnorderedListTypes.Asteriks)
+                                     .AddItem("item 1")
+                                     .AddItem("item 2")
+                                     .AddItem("item 3")
+                                     .AddItem("item 4")
+                                     .AddItem("item 5")
+                                     .AddItem("item 1")
+                                     .AddItem("item 2")
+                                     .AddItem("item 3")
+                                     .AddItem("item 4")
+                                     .AddItem("item 5")
+                                     .AddItem("item 4")
+                                     .AddItem("item 5")
+                                     .AddItem("item 1")
+                                     .AddItem("item 2")
+                                     .AddItem("item 3")
+                                     .AddItem("item 4")
+                                     .AddItem("item 5").Create();
+
+            listNote.Color = TextColor.Blue;
+            Console.ForegroundColor = (ConsoleColor)Enum.Parse(typeof(ConsoleColor),listNote.Color.ToString());
+
+            Console.WriteLine(listNote.View());
+
+            Console.ReadKey();
+        }
+    }
+}
